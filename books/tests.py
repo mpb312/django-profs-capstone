daafd55606d33ee8328112888f05813cdef91192
mpb312 <mpb312@gmail.com>
2023-10-08 13:47:09 +0100

    'ch12'

--- books/tests.py
+++ books/tests.py
@@ -1,22 +1,34 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from .models import Book
+from .models import Book, Review
 
 class BookTests(TestCase):
     @classmethod
     def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user( # new
+            username="reviewuser",
+            email="reviewuser@email.com",
+            password="testpass123",
+        )
         cls.book = Book.objects.create(
             title="Harry Potter",
             author="JK Rowling",
             price="25.00",
         )
+        cls.review = Review.objects.create( # new
+            book=cls.book,
+            author=cls.user,
+            review="An excellent review",
+        )
+
 
     def test_book_listing(self):
         self.assertEqual(f"{self.book.title}", "Harry Potter")
         self.assertEqual(f"{self.book.author}", "JK Rowling")
         self.assertEqual(f"{self.book.price}", "25.00")
-        
+
     def test_book_list_view(self):
         response = self.client.get(reverse("book_list"))
         self.assertEqual(response.status_code, 200)
@@ -29,4 +41,5 @@ class BookTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "Harry Potter")
+        self.assertContains(response, "An excellent review")
         self.assertTemplateUsed(response, "books/book_detail.html")
