b38f6ac76d334f9d9c83e398cf136ddec3b8cabe
mpb312 <mpb312@gmail.com>
2023-10-14 13:02:50 +0100

    ch17

--- django_project/urls.py
+++ django_project/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('anything-but-admin/', admin.site.urls),
     # User management
     path("accounts/", include("allauth.urls")),
     # Local apps
