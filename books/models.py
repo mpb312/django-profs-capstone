e603e2285e5d633ce08ab885d8746479cdf2f919
mpb312 <mpb312@gmail.com>
2023-10-14 12:29:36 +0100

    ch16

--- books/models.py
+++ books/models.py
@@ -14,7 +14,10 @@ class Book(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     cover = models.ImageField(upload_to="covers/", blank = True)
 
-    class Meta: # new
+    class Meta:
+        indexes = [
+            models.Index(fields=["id"], name="id_index"),
+        ]
         permissions = [
             ("special_status", "Can read all books"),
         ]
